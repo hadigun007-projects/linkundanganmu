423109392d3f9374a6c2e9f3b81d182c84332a6f
hadi gunawan <jtgunawan007@gmail.com>
2026-09-14 00:50:45 +0700

    fix: remove wave divider effect from landing page

--- home/src/components/FloralDecorations.tsx
+++ home/src/components/FloralDecorations.tsx
@@ -15,29 +15,6 @@ export default function FloralDecorations() {
         />
       </div>
 
-      {/* 2. SOFT CURVED WAVE DIVIDER (Kurva pembatas bawah putih halus persis gambar referensi) */}
-      <div className="absolute bottom-0 left-0 right-0 w-full overflow-hidden leading-none z-10 pointer-events-none">
-        <svg
-          viewBox="0 0 1440 120"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="relative block w-full h-14 sm:h-20 text-[#FCFBF7]"
-          preserveAspectRatio="none"
-        >
-          {/* Lapisan Ombak Transparan Halus */}
-          <path
-            d="M0,35 C280,95 540,15 820,60 C1100,105 1300,20 1440,55 L1440,120 L0,120 Z"
-            fill="currentColor"
-            opacity="0.45"
-          />
-          {/* Lapisan Ombak Utama (Solid) */}
-          <path
-            d="M0,60 C320,115 620,30 920,75 C1180,115 1350,45 1440,65 L1440,120 L0,120 Z"
-            fill="currentColor"
-          />
-        </svg>
-      </div>
-
     </div>
   );
 }
